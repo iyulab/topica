f5463f518c02f99c1d287a2312331da7067433d5
zizixmas <zizixmas@gmail.com>
2026-05-20 10:01:37 +0900

    feat: react-i18next 설치, i18n 초기화, 빈 로케일 파일 생성

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./lib/i18n";
 import("@iyulab/u-widgets/charts");
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
